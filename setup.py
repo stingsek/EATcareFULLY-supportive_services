7078e7ab581f9b84f01e48663cf17cd29c84fad5
stingsek <michalzadelek10@gmail.com>
2024-11-07 01:18:49 +0100

    First data cleaning applied

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="recommendation_system",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "jupyter",
+        "openfoodfacts",
+        "notebook"
+    ],
+    python_requires=">=3.8",
+    description="recommendation system for food products",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
